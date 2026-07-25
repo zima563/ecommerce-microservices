1308ed320f7563f7d1c7ecc3cf05f4794a28c301
zima563 <mohamedabdelazim654@gmail.com>
2026-07-25 04:39:22 +0300

    feat(rabbitmq): integrate RabbitMQ library and update dependencies

---
+++ libs/rabbitmq/src/rabbitmq.module.ts
@@ -0,0 +1,41 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+import { RabbitmqService } from './rabbitmq.service';
+import { RabbitMQModuleOptions } from './interfaces/rabbitmq-module-options.interface';
+import { RABBITMQ_CLIENT } from './constants/rabbitmq.constants';
+
+@Module({})
+export class RabbitmqModule {
+  static register(options: RabbitMQModuleOptions): DynamicModule {
+    return {
+      module: RabbitmqModule,
+      imports: [ConfigModule],
+      providers: [
+        {
+          provide: 'RABBITMQ_OPTIONS',
+          useValue: options,
+        },
+        {
+          provide: RABBITMQ_CLIENT,
+          inject: [ConfigService, 'RABBITMQ_OPTIONS'],
+          useFactory: (
+            configService: ConfigService,
+            rabbitmqOptions: RabbitMQModuleOptions,
+          ) =>
+            ClientProxyFactory.create({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.getOrThrow<string>('RABBITMQ_URL')],
+                queue: rabbitmqOptions.queue,
+                queueOptions: { durable: true },
+              },
+            }),
+        },
+        RabbitmqService,
+      ],
+      exports: [RabbitmqService],
+    };
+  }
+}
---
+++ libs/rabbitmq/src/rabbitmq.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { RABBITMQ_CLIENT } from './constants/rabbitmq.constants';
+
+@Injectable()
+export class RabbitmqService {
+  constructor(
+    @Inject(RABBITMQ_CLIENT)
+    private readonly client: ClientProxy,
+  ) {}
+
+  emit<T>(pattern: string, data: T) {
+    return this.client.emit(pattern, data);
+  }
+
+  async send<TResult, TData>(pattern: string, data: TData): Promise<TResult> {
+    return firstValueFrom(this.client.send<TResult, TData>(pattern, data));
+  }
+}
